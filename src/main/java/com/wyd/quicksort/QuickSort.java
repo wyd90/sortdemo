9bf6435821e008ccb3a2908a54962e8b3b933cb6
wyd <thuaaaaabb@126.com>
2024-08-17 17:14:02 +0800

    add quick sort

--- src/main/java/com/wyd/quicksort/QuickSort.java
+++ src/main/java/com/wyd/quicksort/QuickSort.java
@@ -1,5 +1,47 @@
 package com.wyd.quicksort;
 
+import java.util.Random;
+
 public class QuickSort {
-    
+    private QuickSort(){}
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        Random random = new Random();
+        sort(arr, 0, arr.length - 1,random);
+    }
+
+    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r, Random rnd) {
+        if (l >= r) {
+            return;
+        }
+
+        int p = partition(arr, l, r, rnd);
+        sort(arr, l, p - 1, rnd);
+        sort(arr, p + 1, r, rnd);
+
+    }
+
+    private static <E extends Comparable<E>> int partition(E[] arr, int l, int r, Random rnd) {
+        // arr[l+1...j] < v; arr[j+1...i] >= v
+
+        int p = l + rnd.nextInt(r - l + 1);
+
+        swap(arr, l , p);
+
+        int j = l;
+        for (int i = l + 1; i <= r; i++) {
+            if (arr[i].compareTo(arr[l]) < 0) {
+                j ++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, l, j);
+        return j;
+    }
+
+    private static <E extends Comparable<E>> void swap(E[] arr, int i, int j) {
+        E t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
 }
